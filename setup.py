04ac7819430f12d648e8de7c9875f5d91268a7dc
Eric McNiece <emcniece@gmail.com>
2020-08-19 22:42:51 -0700

    Parameterized setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,46 @@
 from os import path
 from distutils.core import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+import sys
+from subprocess import Popen, PIPE
+import subprocess
+
+repo_url = 'https://github.com/emcniece/bchydro'
+
+# Fetch latest tag from Github
+cmd = f"git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' {repo_url} | tail -n 1 | cut -d'/' -f3-"
+ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+latest_tag = ps.communicate()[0].decode('utf-8')
+
+# Pull PyPi description from README.md
+pwd = path.abspath(path.dirname(__file__))
+with open(path.join(pwd, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+# Pull requirements from local repo
+with open(path.join(pwd, './requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="bchydro",
     packages=["bchydro"],
-    version="0.6",
+    version=latest_tag,
     license="MIT",
     description="BCHydro API",
-    long_description=long_description,
     long_description_content_type='text/markdown',
+    long_description=long_description,
     author="Eric McNiece",
     author_email="emcniece@gmail.com",
-    url="https://github.com/emcniece/bchydro",
+    url=repo_url,
     download_url="https://github.com/emcniece/bchydro/releases/latest/download/package.tar.gz",
     keywords=["bchydro"],
-    install_requires=["beautifulsoup4"],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
     ],
 )
