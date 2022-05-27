faee39fc1a40d3c55785f482f4cf50d427366d92
Eric McNiece <emcniece@gmail.com>
2022-05-26 20:32:19 -0700

    Functional auth flow

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import os
 import asyncio
 from bchydro import BCHydroApi
 
+LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+logging.basicConfig(level=LOGLEVEL)
+logger = logging.getLogger(__name__)
 
 async def main():
     a = BCHydroApi(os.environ.get("BCH_USER"), os.environ.get("BCH_PASS"), cache_ttl=10)
