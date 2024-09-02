2e0790016d009b9631974402a21a645aa312457b
Dawid Laszuk <laszukdawid@gmail.com>
2024-09-01 20:19:14 -0700

    lint: missing file

--- test_api_simple.py
+++ test_api_simple.py
@@ -6,15 +6,16 @@ from bchydro import BCHydroApiSimple
 
 
 async def main(bep=None):
-    client = BCHydroApiSimple(os.environ.get("BCH_USER"), os.environ.get("BCH_PASS"), browser_exec_path=bep)
+    client = BCHydroApiSimple(
+        os.environ.get("BCH_USER"), os.environ.get("BCH_PASS"), browser_exec_path=bep
+    )
     usage = await client.get_usage_table()
     print(usage)
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='BCHydro API')
-    parser.add_argument('--bep', help='Path to browser executable path')
+    parser = argparse.ArgumentParser(description="BCHydro API")
+    parser.add_argument("--bep", help="Path to browser executable path")
     args = parser.parse_args()
 
     asyncio.get_event_loop().run_until_complete(main(bep=args.bep))
